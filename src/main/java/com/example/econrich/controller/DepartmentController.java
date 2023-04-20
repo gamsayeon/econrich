bad7eef395ddbae75bcfecb167273004bd79c64d
gamsayeon <aud4551@naver.com>
2023-04-20 20:52:16 +0900

    ENH:로그 관리 (Log4j2)

--- src/main/java/com/example/econrich/controller/DepartmentController.java
+++ src/main/java/com/example/econrich/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.example.econrich.controller;
 import com.example.econrich.dto.DepartmentLocationDTO;
 import com.example.econrich.service.Impl.DepartmentServiceImpl;
 import com.example.econrich.service.Impl.EmployeeServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class DepartmentController {
     private final DepartmentServiceImpl departmentService;
     private final EmployeeServiceImpl employeeService;
+    private final Logger logger = LogManager.getLogger(DepartmentController.class);
 
     public DepartmentController(DepartmentServiceImpl departmentService, EmployeeServiceImpl employeeService) {
         this.departmentService = departmentService;
@@ -20,18 +23,21 @@ public class DepartmentController {
 
     @GetMapping("/{departmentId}")
     public ResponseEntity<DepartmentLocationDTO> getDepartmentById(@PathVariable("departmentId") Long departmentId) {
+        logger.debug("부서 번호 " + departmentId + "의 부서 및 위치 정보를 조회합니다.");
         DepartmentLocationDTO departmentLocationDTO = departmentService.getDepartmentById(departmentId);
         return ResponseEntity.ok(departmentLocationDTO);
     }
 
     @GetMapping(params = "name")
     public ResponseEntity<DepartmentLocationDTO> getDepartmentByName(@RequestParam("name") String departmentName) {
+        logger.debug("부서 이름 " + departmentName + "의 부서 및 위치 정보를 조회합니다.");
         DepartmentLocationDTO departmentLocationDTO = departmentService.getDepartmentByName(departmentName);
         return ResponseEntity.ok(departmentLocationDTO);
     }
 
-    @PutMapping("/{departmentId}/employees")
+    @PatchMapping("/{departmentId}/employees")
     public void updateSalaryAndInfoByDepartment(@PathVariable("departmentId") Long departmentId){
+        logger.debug("부서 번호 " + departmentId + "의 급여를 인상 및 사원 정보 업데이트합니다.");
         employeeService.updateSalaryAndInfoByDepartment(departmentId);
     }
 }
--- src/main/java/com/example/econrich/controller/EmployeeController.java
+++ src/main/java/com/example/econrich/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.econrich.controller;
 
 import com.example.econrich.dto.CurrentEmployeeDTO;
 import com.example.econrich.service.Impl.EmployeeServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/employees")
 public class EmployeeController {
     private final EmployeeServiceImpl employeeService;
+    private final Logger logger = LogManager.getLogger(EmployeeController.class);
 
     public EmployeeController(EmployeeServiceImpl employeeService) {
         this.employeeService = employeeService;
@@ -17,12 +20,14 @@ public class EmployeeController {
 
     @GetMapping("/{employeeId}")
     public ResponseEntity<CurrentEmployeeDTO> getEmployeeById(@PathVariable("employeeId") Long employeeId) {
+        logger.debug("사원 번호 " + employeeId + "의 현재 정보를 조회합니다.");
         CurrentEmployeeDTO currentEmployeeDTO = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(currentEmployeeDTO);
     }
 
     @GetMapping(params = "email")
     public ResponseEntity<CurrentEmployeeDTO> getEmployeeByEmail(@RequestParam("email") String email) {
+        logger.debug("사원 이메일 " + email + "의 현재 정보를 조회합니다.");
         CurrentEmployeeDTO currentEmployeeDTO  = employeeService.getEmployeeByEmail(email);
         return ResponseEntity.ok(currentEmployeeDTO);
     }
@@ -30,6 +35,7 @@ public class EmployeeController {
     @GetMapping(params = {"firstName", "lastName"})
     public ResponseEntity<CurrentEmployeeDTO> getEmployeeByName(@RequestParam("firstName") String firstName,
                                                                 @RequestParam("lastName") String lastName) {
+        logger.debug("사원 이름 " + firstName + lastName + "의 현재 정보를 조회합니다.");
         CurrentEmployeeDTO currentEmployeeDTO  = employeeService.getEmployeeByName(firstName, lastName);
         return ResponseEntity.ok(currentEmployeeDTO);
     }
