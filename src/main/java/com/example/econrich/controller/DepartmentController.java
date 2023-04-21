e63f534b2a4b06c7d76b5e717da61ac766926398
gamsayeon <aud4551@naver.com>
2023-04-21 15:05:17 +0900

    ENH:API 가이드 문서 작성 (Swagger)

--- src/main/java/com/example/econrich/controller/DepartmentController.java
+++ src/main/java/com/example/econrich/controller/DepartmentController.java
@@ -3,6 +3,11 @@ package com.example.econrich.controller;
 import com.example.econrich.dto.DepartmentLocationDTO;
 import com.example.econrich.service.Impl.DepartmentServiceImpl;
 import com.example.econrich.service.Impl.EmployeeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
+@Tag(name = "부서 및 위치 정보 조회 API + 특정 부서 급여 인상 API")
 public class DepartmentController {
     private final DepartmentServiceImpl departmentService;
     private final EmployeeServiceImpl employeeService;
@@ -22,6 +28,8 @@ public class DepartmentController {
     }
 
     @GetMapping("/{departmentId}")
+    @Operation(summary = "department_id 으로 조회", description = "department_id로 부서 및 위치 정보를 조회합니다.")
+    @Parameter(name = "departmentId", description = "조회할 부서의 Id", example = "80")
     public ResponseEntity<DepartmentLocationDTO> getDepartmentById(@PathVariable("departmentId") Long departmentId) {
         logger.debug("부서 번호 " + departmentId + "의 부서 및 위치 정보를 조회합니다.");
         DepartmentLocationDTO departmentLocationDTO = departmentService.getDepartmentById(departmentId);
@@ -29,6 +37,8 @@ public class DepartmentController {
     }
 
     @GetMapping(params = "name")
+    @Operation(summary = "department_name 으로 조회", description = "department_name 으로 부서 및 위치 정보를 조회합니다.")
+    @Parameter(name = "name", description = "조회할 부서의 이름", example = "Administration")
     public ResponseEntity<DepartmentLocationDTO> getDepartmentByName(@RequestParam("name") String departmentName) {
         logger.debug("부서 이름 " + departmentName + "의 부서 및 위치 정보를 조회합니다.");
         DepartmentLocationDTO departmentLocationDTO = departmentService.getDepartmentByName(departmentName);
@@ -36,6 +46,8 @@ public class DepartmentController {
     }
 
     @PatchMapping("/{departmentId}/employees")
+    @Operation(summary = "특정 부서의 사원 급여 인상", description = "department_id로 부서를 조회하여 각 사원의 급여를 인상합니다.")
+    @Parameter(name = "departmentId", description = "급여를 인상할 부서의 Id", example = "80")
     public void updateSalaryAndInfoByDepartment(@PathVariable("departmentId") Long departmentId){
         logger.debug("부서 번호 " + departmentId + "의 급여를 인상 및 사원 정보 업데이트합니다.");
         employeeService.updateSalaryAndInfoByDepartment(departmentId);
